27d0b4a16214bac19881982a2dbf56f0020ecdb1
Beatriz <biafigmonteiro@gmail.com>
2024-11-14 16:35:30 -0300

    (build)[Frontend] Add primevue and primeicons (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primevue/themes/aura";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: ['@primevue/nuxt-module'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  primevue: {
+    components: {
+      include: '*'
+    },
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'white',
+          cssLayer: false
+        }
+      }
+    }
+  }
 })
